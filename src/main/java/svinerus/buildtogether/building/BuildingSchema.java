71b37689dd14d5f310150ce1603e85dcdcbe3840
krolik1591 <dasenkosasa19@gmail.com>
2022-07-28 13:49:29 +0300

    add method `blocksLocation` and `blocksCount`

--- src/main/java/svinerus/buildtogether/building/BuildingSchema.java
+++ src/main/java/svinerus/buildtogether/building/BuildingSchema.java
@@ -6,9 +6,7 @@ import com.sk89q.worldedit.regions.CuboidRegion;
 import org.bukkit.Material;
 import org.bukkit.World;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
 
 
 public class BuildingSchema {
@@ -26,19 +24,41 @@ public class BuildingSchema {
         this.layers.add(layer);
     }
 
+    public HashMap<Material, Integer> blocksCount() {
+        HashMap<Material, Integer> result = new HashMap<>();
+        for (var layer : layers) {
+            for (var material : layer.blocks.values()) {
+                if (!result.containsKey(material))
+                    result.put(material, 0);
+                result.put(material, result.get(material) + 1);
+            }
+        }
+        return result;
+    }
+
+    public Set<BlockVector3> blocksLocation() {
+        Set<BlockVector3> result = new HashSet<>();
+        for (var layer : layers)
+            result.addAll(layer.blocks.keySet());
+        return result;
+    }
+
     public World world() {
         return BukkitAdapter.adapt(Objects.requireNonNull(region.getWorld()));
     }
 
     public static class Layer {
         HashMap<BlockVector3, Material> blocks;
+
         public Layer(HashMap<BlockVector3, Material> blocks) {
             this.blocks = blocks;
         }
+
         public HashMap<BlockVector3, Material> blocks() {
             return blocks;
         }
     }
+
 }
 
 
